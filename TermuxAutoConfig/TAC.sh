8c877c23ad4dc290be3bc612ec108e95c326e4cb
Atopes Sayuri <108107785+AtopesSayuri@users.noreply.github.com>
2024-03-19 01:03:53 +0800

    push

--- TermuxAutoConfig/TAC.sh
+++ TermuxAutoConfig/TAC.sh
@@ -3,8 +3,11 @@
 
 set -x
 
+# 部分准备
+termux-setup-storage
+termux-wake-lock
+CWD="$(pwd)"
 TMP=AtopesTMP
-
 mkdir -p ${TMP}
 cd ${TMP}
 
@@ -15,3 +18,18 @@ wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/JetBrainsM
 unzip JBM.zip
 ## 替换字体
 mv JetBrainsMonoNerdFont-Bold.ttf ${HOME}/.termux/font.ttf
+
+# motd 修改
+## 展示时间与用户
+cat <<-EOF >>${PREFIX}/etc/profile
+	LOGT="$(date +"%Y-%m-%d %H:%M:%S")"
+	echo "Time: ${LOGT} | User: $(whoami)@$(hostname)"
+EOF
+
+# 夹点私货
+## 打开项目主页
+termux-open-url https://github.com/AtopesSayuri/Small-Tools
+
+# 复位
+termux-wake-unlock
+cd ${CWD}
